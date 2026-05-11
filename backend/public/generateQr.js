2646ce48f4af4b3ef30e6267ed790c5bf012d0c0
Omar <shaidomarsha@gmail.com>
2026-05-11 11:00:58 +0800

    change apk

--- backend/public/generateQr.js
+++ backend/public/generateQr.js
@@ -3,7 +3,7 @@ const qrcode = require("qrcode-terminal");
 const url = "https://quit-smoking-api-zr94.onrender.com/download.html";
 
 qrcode.generate(url, { small: true }, (qr) => {
-  console.log("\n📱 Scan to open download page:\n");
+  console.log("\n📱 Scan to open and download page:\n");
   console.log(qr);
   console.log("🔗 URL:", url);
 });
